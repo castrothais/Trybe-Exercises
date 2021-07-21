150b3060cd0ca9c31b88d717f1a661e650bcff1f
Thaís Sampaio <thais_castrocs@outlook.com>
2021-07-21 12:12:25 -0300

    Exercicio 1 e 2 Finalizados

---
+++ exercises/M2_Front_End/D.1/exercise-content/test/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Components extends React.Component {
+    render() {
+    return (
+        <div>
+            <h1>Thaís</h1>
+            <p>Estudante de Desenvolvimento Web</p>
+        </div>
+    )
+    }
+}
+export default Components
